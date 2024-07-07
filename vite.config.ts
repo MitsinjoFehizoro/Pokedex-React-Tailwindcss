e2e6423e55033ddd430794b30aad449e048a4947
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-07-07 14:46:40 +0300

    Mis à jour avant le mettre en production

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
